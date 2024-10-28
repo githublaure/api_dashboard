36bda1adc9ceaebbc65d3d44fd22a2493a8d3bbc
githublaure <77734588+githublaure@users.noreply.github.com>
2024-10-28 18:37:38 +0100

    updated api

--- api/api.py
+++ api/api.py
@@ -1,20 +1,21 @@
-from flask import Flask, jsonify
 import pickle
 import pandas as pd
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-app = Flask(__name__)
+app = FastAPI()
 
-# Chargement du modèle
-with open('models/moyenne.pkl', 'rb') as f:
+
+class MoyenneModel:
+    def predict(self, data):
+        return data.mean()
+
+
+with open('../models/moyenne.pkl', 'rb') as f:
     model = pickle.load(f)
 
-@app.route('/score', methods=['GET'])
-def get_score():
-    df = pd.read_csv('data/processed/data_test.csv')
+@app.get('/score')
+async def get_score():
+    df = pd.read_csv('../data/processed/data_test.csv')
     scores = model.predict(df['Notes'])
-    return jsonify({'moyenne': scores})
-
-if __name__ == '__main__':
-    # host='0.0.0.0' : Cela permet à l'application d'écouter sur toutes les interfaces réseau, 
-    # ce qui est nécessaire pour permettre l'accès externe à l'API.
-    app.run(debug=True, host='0.0.0.0', port=5000) #port=5000 : Définit le port sur lequel l'application API écoutera
+    return JSONResponse(content={'moyenne': scores.tolist()})
