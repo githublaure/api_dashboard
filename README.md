9ce2a1a4de9248589b30e6371c616e2d75e95ee5
githublaure <77734588+githublaure@users.noreply.github.com>
2024-10-29 22:59:48 +0100

    added comments to tests

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # api_dashboard
+/votre_projet/
+├── api/
+│   ├── api.py
+│   └── models.py
+├── dashboard/
+│   └── dashboard.py
+├── data/
+│   └── processed/
+│       └── data_test.csv
+├── tests/                # Dossier pour les tests
+│   └── test_api.py      # Tests pour l'API
+└── requirements.txt
--- tests/test_api.py
+++ tests/test_api.py
@@ -6,18 +6,18 @@ import pytest
 
 client = TestClient(app)
 
-def test_read_root():
+def test_read_root():#Tests de Valeur, Vérifie que la racine de l'API renvoie le bon message et le code de statut 200.
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Bienvenue à l'API de calcul de moyenne!"}
 
-def test_get_score_valid():
+def test_get_score_valid(): #Tests de Valeur, Vérifie que l'API renvoie un code de statut 200 et que le format de la réponse est correct (une liste pour la moyenne).
     response = client.get("/score")
     assert response.status_code == 200
     data = response.json()
     assert isinstance(data['moyenne'], list)  # Vérifier que la moyenne est une liste
 
-def test_get_score_empty_dataset(mocker):
+def test_get_score_empty_dataset(mocker):#Tests d'État simule un DataFrame vide et vérifie que la réponse renvoie une liste vide.
     # Mocker la lecture du fichier pour simuler un dataset vide
     mocker.patch('pandas.read_csv', return_value=pd.DataFrame(columns=['Élève', 'Notes']))
     response = client.get("/score")
@@ -25,11 +25,11 @@ def test_get_score_empty_dataset(mocker):
     data = response.json()
     assert data['moyenne'] == []  # Vérifiez que la moyenne renvoyée est vide
 
-def test_get_score_invalid_endpoint():
+def test_get_score_invalid_endpoint():#Tests d'État, vérifie qu'une requête à un endpoint inexistant renvoie une erreur 404.
     response = client.get("/invalid")
     assert response.status_code == 404  # Vérifiez que l'endpoint inexistant renvoie une erreur 404
 
-def test_predict_exception():
+def test_predict_exception():# Test d'exception, vérifie qu'une exception est levée si le modèle reçoit des données invalides (comme None).
     # Exemple d'une fonction qui pourrait lever une exception
     with pytest.raises(ValueError):
         model = MoyenneModel()
